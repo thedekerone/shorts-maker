4f06e8d82178543bbcb08c5b43a2278e7eca0e8d
Mauricio Fow <mauricio.fow@quanturi.com>
2025-01-24 21:48:41 +0200

    back to rs

--- handlers/replicate.go
+++ handlers/replicate.go
@@ -515,7 +515,6 @@ func uploadToMinio(jobID string, minioClient *services.MinioService, outputFileP
 
 func getImagesWithTimestamps(transcript *models.TranscriptionOutput, script string, numImages int32) ([]models.ImageWithTimestamp, error) {
 	rs, err := services.NewReplicateService()
-	deepseek, err := services.NewDeepSeekService()
 	if err != nil {
 		return nil, fmt.Errorf("error creating replicate service: %w", err)
 	}
@@ -559,7 +558,7 @@ Key rules:
 		segmentStrings = segmentStrings + fmt.Sprintf("{ segment: %s, start: %.3f, end: %.3f } \n", v.Text, v.Start, v.End)
 	}
 
-	promptForImage, err := deepseek.
+	promptForImage, err := rs.
 		GetCompletitionForImages(imageGenerationPrompts+fmt.Sprintf("\n %v", segmentStrings), "")
 
 	println("%v", promptForImage)
