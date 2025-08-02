b83a838395bdc6ed35c3794e67fa11089734eff7
Mauricio <mauriciofow@opslayer.com>
2025-08-02 21:26:28 +0300

    Sa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo "$WI_CONFIG" > /tmp/wi-config.json
+# write the SA key to a file that only this container can read
+echo "$SA_JSON" > /tmp/adc.json
+chmod 400 /tmp/adc.json
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/adc.json
 exec "$@"
