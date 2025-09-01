c4335fce23b6075c009583b096731dcb2b35d6de
Mauricio <mauriciofow@opslayer.com>
2025-09-02 00:17:24 +0300

    use google images

--- services/replicate.go
+++ services/replicate.go
@@ -86,7 +86,7 @@ func (rs *ReplicateService) GetCompletition(prompt string, systemPrompt string) 
 
 func (rs *ReplicateService) GetImages(prompt string, quantity int64, mode string) ([]string, error) {
 	ctx := context.TODO()
-	model := "stability-ai/stable-diffusion-3"
+	model := "google/imagen-4-fast"
 
 	aspect_ratio := "9:16"
 
